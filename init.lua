f827d9d52c985fc6f4fe9368ca3c5fcd7faea119
irregularBismuth <118374482+irregularBismuth@users.noreply.github.com>
2025-11-25 22:44:00 +0100

    add proc macro support

--- init.lua
+++ init.lua
@@ -648,7 +648,20 @@ require('nixCatsUtils.lazyCat').setup(nixCats.pawsible { 'allPlugins', 'start', 
       -- servers.clangd = {},
       -- servers.gopls = {},
       -- servers.pyright = {},
-      -- servers.rust_analyzer = {},
+      servers.rust_analyzer = {
+          settings = {
+        ["rust-analyzer"] = {
+      cargo = {
+        buildScripts = {
+          enable = true,
+        },
+      },
+      procMacro = {
+        enable = true,
+      },
+    },
+        }
+      },
       -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
       --
       -- Some languages (like typescript) have entire language plugins that can be useful:
