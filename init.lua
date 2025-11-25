021f9482f1b3e77c7892cde6fbbb5083475a8c99
irregularBismuth <118374482+irregularBismuth@users.noreply.github.com>
2025-11-25 23:08:08 +0100

    update init lua

--- init.lua
+++ init.lua
@@ -644,24 +644,26 @@ require('nixCatsUtils.lazyCat').setup(nixCats.pawsible { 'allPlugins', 'start', 
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       -- NOTE: nixCats: there is help in nixCats for lsps at `:h nixCats.LSPs` and also `:h nixCats.luaUtils`
-      local servers = {}
-      -- servers.clangd = {},
-      -- servers.gopls = {},
-      -- servers.pyright = {},
-      servers.rust_analyzer = {
-          settings = {
-        ["rust-analyzer"] = {
-      cargo = {
-        buildScripts = {
+    local servers = {
+  -- clangd = {},
+  -- gopls = {},
+  -- pyright = {},
+
+  rust_analyzer = {
+    settings = {
+      ["rust-analyzer"] = {
+        cargo = {
+          buildScripts = {
+            enable = true,
+          },
+        },
+        procMacro = {
           enable = true,
         },
       },
-      procMacro = {
-        enable = true,
-      },
     },
-        }
-      },
+  },
+}
       -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
       --
       -- Some languages (like typescript) have entire language plugins that can be useful:
